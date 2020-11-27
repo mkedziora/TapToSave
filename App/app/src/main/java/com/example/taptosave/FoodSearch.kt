084d3aeae561f2f22bee6ea6eac512613e574a66
Rogalsonn <kuba1b14@gmail.com>
2020-11-28 00:57:42 +0100

    parser, csvki

---
+++ App/app/src/main/java/com/example/taptosave/FoodSearch.kt
@@ -0,0 +1,10 @@
+package com.bi.chartapp
+
+import com.example.taptosave.R
+
+data class FoodSearch(
+    val id: String,
+    val googleTopic: String,
+    val week_id: String,
+    val value: Int
+)
