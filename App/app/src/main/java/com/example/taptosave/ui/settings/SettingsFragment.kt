d6fdbf5ce93fc494b06be160d1829dd4930ad030
Jędrzej <jedrzejleder@gmail.com>
2020-11-28 01:36:13 +0100

    Settings improvement

--- App/app/src/main/java/com/example/taptosave/ui/settings/SettingsFragment.kt
+++ App/app/src/main/java/com/example/taptosave/ui/settings/SettingsFragment.kt
@@ -1,16 +1,12 @@
 package com.example.taptosave.ui.settings
 
-import android.app.Activity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.taptosave.R
-import android.widget.Button
-import android.widget.Toast
-import android.widget.EditText
 import kotlinx.android.synthetic.main.fragment_settings.view.*
 
 class SettingsFragment: Fragment() {
@@ -23,12 +19,8 @@ class SettingsFragment: Fragment() {
 
         val button = root.buttonConfirm
         val buttonConnect = root.buttonConnect
-        val edit = root.editTextInhabitants
-        val editTarget = root.editTextTarget
          button.setOnClickListener {
              Toast.makeText(getActivity(), "Powodzenia!", Toast.LENGTH_LONG).show();
-             edit.text.clear()
-             editTarget.text.clear()
          }
          buttonConnect.setOnClickListener {
              Toast.makeText(getActivity(), "Brak urządzeń w zasięgu", Toast.LENGTH_LONG).show();
