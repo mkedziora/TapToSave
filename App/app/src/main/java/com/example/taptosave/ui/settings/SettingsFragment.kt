580808da8ad2be9525c210ff7c0d08d466744276
Jędrzej <jedrzejleder@gmail.com>
2020-11-28 12:47:57 +0100

    Scoreboard design

--- App/app/src/main/java/com/example/taptosave/ui/settings/SettingsFragment.kt
+++ App/app/src/main/java/com/example/taptosave/ui/settings/SettingsFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.taptosave.R
-import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_settings.view.*
 
 class SettingsFragment: Fragment() {
